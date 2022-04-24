e3ee8a2c91c88dfe880f362709a263d1924ca3ea
Wadim Martinkevich <martinkewichwadim@gmail.com>
2022-04-24 23:39:23 +0300

    fix: update border

--- js/goTop.js
+++ js/goTop.js
@@ -20,7 +20,7 @@
       }
     }
   
-    var goTopBtn = document.querySelector('.back-top');
+    let goTopBtn = document.querySelector('.back-top');
   
     window.addEventListener('scroll', trackScroll);
     goTopBtn.addEventListener('click', backToTop);
